d7dcf8aed51f8a4d554d4ae83d35bb4c49240113
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-09-28 20:43:14 -0400

    there were number skips

--- batchBatchTwo.py
+++ batchBatchTwo.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 
 text = open("roomList.txt", "rt").read()
+count = -1
 for root, dirs, files in os.walk("./mapooFiles"):
     for file in files:
-        if (int(file[0:4]) >= 570) and (int(file[0:4]) <= 1126):
+        if (int(file[0:4]) >= 570) and (int(file[0:4]) <= 1146):
+            count = count + 1
             subprocess.run([ "lzss.exe", "-d", os.path.join(root, file) ])
-            os.rename("./mapooFiles/" + file, "./mapooFiles/" + file[0:4] + " [" + text.split("\n")[int(file[0:4]) - 570] + "].bin")
+            os.rename("./mapooFiles/" + file, "./mapooFiles/" + file[0:4] + " [" + text.split("\n")[count] + "].bin")
