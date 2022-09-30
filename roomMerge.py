af3a4b31dea3dd77cb0ce3b8460de62caebc900b
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-09-30 11:19:40 -0400

    can merge room files, probably will need this

---
+++ roomMerge.py
@@ -0,0 +1,10 @@
+import os
+new = open("output_roominfo.bin", "ab")
+old = open("roominfo.bin", "rb").read()
+new.write(old[0:7168])
+for root, dirs, files in os.walk("./roomFiles"):
+    sortList = files
+    sortList.sort()
+    for file in sortList:
+        new.write(open("./roomFiles/" + file, "rb").read())
+new.write(old[1267328:])
